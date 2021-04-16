1c629ef4a2dc7bdb82707d5bccc23489bd782807
Jaime Baires <jebaires1999@gmail.com>
2021-04-16 12:35:52 +0200

    Realizados los test del método precio() y constructor de la clase Seguro. Falta completar plan de pruebas (pruebas del constructor).

--- P4/src/main/java/es/unican/is2/seguros/model/Seguro.java
+++ P4/src/main/java/es/unican/is2/seguros/model/Seguro.java
@@ -9,46 +9,51 @@ public class Seguro {
 	private Cobertura cobertura;
 	private Cliente cliente;
 
-	public Seguro(int potenciaCV, Cobertura cobertura, Cliente cliente) {
+	public Seguro(int potenciaCV, Cobertura cobertura, Cliente cliente, LocalDate fecha) throws DatoIncorrectoException {
+		if (potenciaCV < 0)
+			throw new DatoIncorrectoException();
+		if (cobertura == null)
+			throw new DatoIncorrectoException();
+		if (fecha == null)
+			throw new DatoIncorrectoException();
+		if(fecha.compareTo(LocalDate.now()) > 0)
+			throw new DatoIncorrectoException();
 		this.potenciaCV = potenciaCV;
 		this.cobertura = cobertura;
 		this.cliente = cliente;
+		this.fechaUltimoSiniestro = fecha;
 	}
 
 	public double precio() throws DatoIncorrectoException {
-		return 2;
-	}
-
-	public LocalDate getFechaUltimoSiniestro() {
-		return fechaUltimoSiniestro;
-	}
-
-	public void setFechaUltimoSiniestro(LocalDate fechaUltimoSiniestro) {
-		this.fechaUltimoSiniestro = fechaUltimoSiniestro;
-	}
-
-	public int getPotenciaCV() {
-		return potenciaCV;
-	}
-
-	public void setPotenciaCV(int potenciaCV) {
-		this.potenciaCV = potenciaCV;
-	}
-
-	public Cobertura getCobertura() {
-		return cobertura;
-	}
-
-	public void setCobertura(Cobertura cobertura) {
-		this.cobertura = cobertura;
-	}
-
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
+		
+		double precio = 0;
+		
+		if(this.cobertura == Cobertura.TODORIESGO) {
+			precio += 1000;
+		} else if(this.cobertura == Cobertura.TERCEROSLUNAS) {
+			precio += 600;
+		} else {
+			precio += 400;
+		}
+		
+		if(this.potenciaCV>=90 && this.potenciaCV<=110) {
+			precio *= 1.05;
+		} else if(this.potenciaCV>110) {
+			precio *= 1.2;
+		}
+		
+		LocalDate fechaActual = LocalDate.now();
+		if(fechaActual.minusDays(364).compareTo(this.fechaUltimoSiniestro) <= 0) {
+			precio += 200;
+		} else if(fechaActual.minusYears(2).minusDays(364).compareTo(this.fechaUltimoSiniestro) <= 0) {
+			precio += 50;
+		}
+		
+		if(this.cliente.isMinusvalia()) {
+			precio *= 0.75;
+		}
+		System.out.println(precio);
+		return precio;
 	}
 
 }
--- P4/src/test/java/es/unican/is2/seguros/model/SeguroTest.java
+++ P4/src/test/java/es/unican/is2/seguros/model/SeguroTest.java
@@ -1,5 +1,10 @@
 package es.unican.is2.seguros.model;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.time.LocalDate;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,22 +16,141 @@ public class SeguroTest {
 	}
 
 	@Test
-	public void test() {
+	public void PrecioTest() {
+		Seguro sut;
+		Cliente minusvalidoTrue = new Cliente(null, null, true);
+		Cliente minusvalidoFalse = new Cliente(null, null, false);
+		LocalDate fecha;
+		
 		/*
-		 * - Fecha = Actual; Potencia = 0; Cobertura = TERCEROS; Minusvalía = Verdadero
-		 * - Fecha = Actual – 3 meses; Potencia = 45; Cobertura=TODORIESGO;
-		 * minusvalía=falso - Fecha = Actual – 364 días; Potencia = 89; Cobertura =
-		 * TERCEROSLUNA; minusvalía=falso - Fecha = Actual – 1 año; Potencia = 90;
-		 * Cobertura = TERCEROS; minusvalía=falso - Fecha = Actual – 2 años; Potencia =
-		 * 100; Cobertura = TODORIESGO; minusvalía = verdadero - Fecha = Actual – 2 años
-		 * y 364 días; Potencia = 110; Cobertura = TODORIESGO; minusvalía = verdadero -
-		 * Fecha = Actual – 3 años; Potencia = 111; Cobertura = TERCEROS; minusvalía =
-		 * verdadero - Fecha = Actual – 4 años; Potencia = 200; Cobertura=TERCEROSLUNA;
-		 * minusvalía= verdadero
-		 */
-		Cliente verd = new Cliente(null, null, true);
-		Cliente falso = new Cliente(null, null, false);
-		Seguro sut1 = new Seguro(0, Cobertura.TERCEROS, verd);
+		 * CASOS VÁLIDOS
+		    -	Fecha = Actual; Potencia = 0; Cobertura = TERCEROS; Minusvalía = Verdadero; Resultado esperado = 450
+			-	Fecha = Actual – 3 meses; Potencia = 45; Cobertura=TODORIESGO; Minusvalía=falso; Resultado esperado = 1200
+			-	Fecha = Actual – 364 días; Potencia = 89; Cobertura = TERCEROSLUNA; Minusvalía=falso; Resultado esperado = 800
+			-	Fecha = Actual – 1 año; Potencia = 90; Cobertura = TERCEROS; Minusvalía=falso; Resultado esperado = 470
+			-	Fecha = Actual – 2 años; Potencia = 100; Cobertura = TODORIESGO; Minusvalía = verdadero; Resultado esperado = 825
+			-	Fecha = Actual – 2 años y 364 días; Potencia = 110; Cobertura = TODORIESGO; Minusvalía = verdadero; Resultado esperado = 825
+			-	Fecha = Actual – 3 años; Potencia = 111; Cobertura = TERCEROS; Minusvalía = verdadero; Resultado esperado = 360
+			-	Fecha = Actual – 4 años; Potencia = 200; Cobertura=TERCEROSLUNA; Minusvalía= verdadero; Resultado esperado = 540
+		 * */
+
+		try {
+			fecha = LocalDate.now();
+			sut = new Seguro(0, Cobertura.TERCEROS, minusvalidoTrue, fecha);
+			assertTrue(sut.precio()==450);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusMonths(3);
+			sut = new Seguro(45, Cobertura.TODORIESGO, minusvalidoFalse, fecha);
+			assertTrue(sut.precio()==1200);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusDays(364);
+			sut = new Seguro(89, Cobertura.TERCEROSLUNAS, minusvalidoFalse, fecha);
+			assertTrue(sut.precio()==800);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusYears(1);
+			sut = new Seguro(90, Cobertura.TERCEROS, minusvalidoFalse, fecha);
+			assertTrue(sut.precio()==470);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusYears(2);
+			sut = new Seguro(100, Cobertura.TODORIESGO, minusvalidoTrue, fecha);
+			assertTrue(sut.precio()==825);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusYears(2).minusDays(364);
+			sut = new Seguro(110, Cobertura.TODORIESGO, minusvalidoTrue, fecha);
+			assertTrue(sut.precio()==825);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusYears(3);
+			sut = new Seguro(111, Cobertura.TERCEROS, minusvalidoTrue, fecha);
+			assertTrue(sut.precio()==360);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		try {
+			fecha = LocalDate.now().minusYears(4);
+			sut = new Seguro(200, Cobertura.TERCEROSLUNAS, minusvalidoTrue, fecha);
+			assertTrue(sut.precio()==540);
+		} catch (DatoIncorrectoException e) {
+			fail();
+		}
+		
+		
+		/*
+		 * CASOS NO VÁLIDOS
+		    -	Fecha= Actual +1 día; Potencia=0; Cobertura=TODORIESGO; Minusvalía=falso; Resultado esperado = DatoIncorrectoException
+			-	Fecha = Actual +1 año; Potencia = 45; Cobertura= TERCEROS; Minusvalía=verdadero; Resultado esperado = DatoIncorrectoException
+			-	Fecha = Null; Potencia = 45; Cobertura= TODORIESGO; Minusvalía = verdadero; Resultado esperado = DatoIncorrectoException
+			-	Fecha = Actual; Potencia = -1; Cobertura = TERCEROS; Minusvalía = falso; Resultado esperado = DatoIncorrectoException
+			-	Fecha = Actual; Potencia = -30; Cobertura = TERCEROS; Minusvalía = falso; Resultado esperado = DatoIncorrectoException
+			-	Fecha = Actual; Potencia = 45; Cobertura = Null; Minusvalía = falso; Resultado esperado = DatoIncorrectoException
+		 * */
+		
+		try {
+			fecha = LocalDate.now().plusDays(1);
+			sut = new Seguro(0, Cobertura.TODORIESGO, minusvalidoFalse, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			fecha = LocalDate.now().plusYears(1);
+			sut = new Seguro(45, Cobertura.TERCEROS, minusvalidoTrue, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			fecha = null;
+			sut = new Seguro(45, Cobertura.TODORIESGO, minusvalidoTrue, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			fecha = LocalDate.now();
+			sut = new Seguro(-1, Cobertura.TERCEROS, minusvalidoFalse, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			fecha = LocalDate.now();
+			sut = new Seguro(-30, Cobertura.TERCEROS, minusvalidoFalse, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
+		try {
+			fecha = LocalDate.now();
+			sut = new Seguro(45, null, minusvalidoFalse, fecha);
+			sut.precio();
+			fail();
+		} catch (DatoIncorrectoException e) {}
+		
 	}
 
 }
