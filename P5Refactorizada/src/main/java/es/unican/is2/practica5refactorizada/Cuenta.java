80f665f1cfae82d488a65d6cb5112ca15a2a460e
Jaime Baires <jebaires1999@gmail.com>
2021-05-09 20:27:26 +0200

    Práctica 5 - Añadido el proyecto que se refactorizará, y comienzo de las refactorizaciones

---
+++ P5Refactorizada/src/main/java/es/unican/is2/practica5refactorizada/Cuenta.java
@@ -0,0 +1,15 @@
+package es.unican.is2.practica5refactorizada;
+
+public abstract class Cuenta {
+
+	private String numCuenta;
+
+	public Cuenta(String numCuenta) { // WMC = 1, CCog = 0
+		this.numCuenta = numCuenta;
+	}
+
+	public String getNumCuenta() { // WMC = 1, CCog = 0
+		return numCuenta;
+	}
+
+}
---
+++ P5Refactorizada/src/main/java/es/unican/is2/practica5refactorizada/Debito.java
@@ -0,0 +1,47 @@
+package es.unican.is2.practica5refactorizada;
+
+import java.time.LocalDate;
+
+public class Debito extends Tarjeta {
+	
+	private double saldoDiarioDisponible;
+
+	public Debito(String numero, String titular, CuentaAhorro c) { // WMC = 1, CCog = 0
+		super(numero, titular, c);
+	}
+	
+	
+	@Override
+	public void retirar(double x) throws saldoInsuficienteException, datoErroneoException { // WMC = 2, CCog = 1
+		if (saldoDiarioDisponible<x) { // WMC + 1, CCog = 1
+			throw new saldoInsuficienteException("Saldo insuficiente");
+		}
+		this.mCuentaAsociada.retirar("Retirada en cajero automático", x);
+		saldoDiarioDisponible-=x;
+	}
+	
+	@Override
+	public void pagoEnEstablecimiento(String datos, double x) throws saldoInsuficienteException, datoErroneoException { // WMC = 2, CCog = 1
+		if (saldoDiarioDisponible<x) { // WMC + 1, CCog + 1
+			throw new saldoInsuficienteException("Saldo insuficiente");
+		}
+		this.mCuentaAsociada.retirar("Compra en : " + datos, x);
+		saldoDiarioDisponible-=x;
+	}
+	
+	public LocalDate getCaducidadDebito() { // WMC = 1, CCog = 0
+		return this.mCuentaAsociada.getCaducidadDebito();
+	}
+	
+	/**
+	 * Método invocado automáticamente a las 00:00 de cada día
+	 */
+	public void restableceSaldo() { // WMC = 1, CCog = 0
+		saldoDiarioDisponible = mCuentaAsociada.getLimiteDebito();
+	}
+	
+	public CuentaAhorro getCuentaAsociada() { // WMC = 1, CCog = 0
+		return mCuentaAsociada;
+	}
+
+}
